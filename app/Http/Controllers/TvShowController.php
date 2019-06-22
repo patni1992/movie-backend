281cd7fcc4e6219d44213d331b9744ab005a13ed
Patrik <patriknilsson1992@gmail.com>
2019-06-22 21:41:44 +0700

    add TvShowResource

--- app/Http/Controllers/TvShowController.php
+++ app/Http/Controllers/TvShowController.php
@@ -4,39 +4,41 @@ namespace App\Http\Controllers;
 
 use App\TvShow;
 use Illuminate\Http\Request;
+use App\Http\Resources\TvShowResource;
 
 class TvShowController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
      */
     public function index()
     {
-        return TvShow::all();
+        return TvShowResource::collection(TvShow::all());
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \App\Http\Resources\TvShowResource
      */
     public function store(Request $request)
     {
-        return TvShow::create($request->all());
+        $tvShow = TvShow::create($request->all());
+        return new TvShowResource($tvShow);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  \App\TvShow  $tvShow
-     * @return \Illuminate\Http\Response
+     * @return \App\Http\Resources\TvShowResource
      */
     public function show(TvShow $tvShow)
     {
-        return $tvShow;
+        return new TvShowResource($tvShow);
     }
 
     /**
@@ -44,12 +46,12 @@ class TvShowController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\TvShow  $tvShow
-     * @return \Illuminate\Http\Response
+     * @return \App\Http\Resources\TvShowResource
      */
     public function update(Request $request, TvShow $tvShow)
     {
         $tvShow->update($request->all());
-        return $tvShow;
+        return new TvShowResource($tvShow);
     }
 
     /**
---
+++ app/Http/Resources/TvShowResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TvShowResource extends JsonResource
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'summary' => $this->summary,
+            'image' => $this->image,
+            'premiered' => $this->premiered
+        ];
+    }
+}
