6bb5ba6def428763f036721bae89d56136f76b45
Patrik <patriknilsson1992@gmail.com>
2019-06-24 04:49:40 +0700

    add tv_show_id to fillable

--- app/Rating.php
+++ app/Rating.php
@@ -7,7 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Rating extends Model
 {
     protected $fillable = [
-        'rating'
+        'rating',
+        'tv_show_id'
     ];
 
     public function tvShow()
