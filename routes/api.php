5790bd82b545b153b3ff2831df50f74a224529a8
Patrik <patriknilsson1992@gmail.com>
2019-06-25 01:44:56 +0700

    add auth controller implement jwt

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Rating;
+use App\TvShow;
+use App\Http\Resources\TvShowResource;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,10 +14,17 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+ */
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
 });
 
-Route::resource('tv-shows', 'TvShowController');
+Route::apiResource('tv-shows', 'TvShowController');
+Route::apiResource('tv-shows.ratings', 'RatingController');
+
